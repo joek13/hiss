5a155d45d96ca60a3d261247d4b41d415a4bfb01
Joe Kerrigan <joek1301@gmail.com>
2023-06-09 14:19:39 -0400

    Add parseProgram to Syntax

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,9 +1,15 @@
-module Syntax (parseExpression) where
+module Syntax (parseProgram, parseExpression) where
 
 import Error (HissError (SyntaxError))
-import Syntax.AST (Exp)
+import Syntax.AST (Exp, Program)
 import Syntax.Lexer (Range, runAlex)
-import Syntax.Parser (parseExp)
+import Syntax.Parser (parseExp, parseProg)
+
+-- Wraps Lexer.parseProgram and returns HissError in case of failure
+parseProgram :: String -> Either HissError (Program Range)
+parseProgram inp = case runAlex inp parseProg of
+  Left msg -> Left (SyntaxError msg)
+  Right ast -> Right ast
 
 -- Wraps Lexer.parseExp and returns HissError in case of failure
 parseExpression :: String -> Either HissError (Exp Range)
