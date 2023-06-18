74095d2420d866c9b74ebf7141015433ff4ce084
Joe Kerrigan <joek1301@gmail.com>
2023-06-18 10:40:16 -0400

    Refactor AST
    - Add Annotated typeclass and remove Foldable magic
        - Renders current ASTSpec tests obsolete
    - Remove mapAnn functions (replaced by fmap)
    - Add ASTSpec test for Name equality

--- src/Syntax/AST.hs
+++ src/Syntax/AST.hs
@@ -1,117 +1,151 @@
-module Syntax.AST (Program, Decl (..), Name (..), BinOp (..), UnaryOp (..), Binding (..), Exp (..), FunApp (..), getAnn, mapAnn, stripAnns, getIdent, declGetName, declMapAnn, declStripAnns) where
+module Syntax.AST
+  ( Annotated (getAnn),
+    Program (..),
+    Decl (..),
+    Name (..),
+    BinOp (..),
+    UnaryOp (..),
+    Binding (..),
+    Expr (..),
+    progDecls,
+    getIdent,
+    declGetName,
+  )
+where
 
-import Data.Maybe (fromJust)
-import Data.Monoid (getFirst)
 import Data.Ord (comparing)
 
--- a Hiss program is given by zero or more top-level declarations.
-type Program a = [Decl a]
+-- | Class for a syntax node annotated with, e.g., source range information.
+class Annotated n where
+  getAnn :: n a -> a
+  -- ^ Gets an object's annotation.
 
--- Top-level declaration
-data Decl a = Decl a (Binding a) (Exp a)
-  deriving (Eq, Show, Foldable)
+-- | A Hiss program, given by zero or more top-level declarations.
+data Program a = Program a [Decl a]
+  deriving (Eq, Show, Functor)
 
--- Gets Name bound by a declaration.
+-- | Gets a program's list of declarations.
+progDecls :: Program a -> [Decl a]
+progDecls (Program _ decls) = decls
+
+instance Annotated Program where
+  getAnn (Program a _) = a
+
+-- | Top-level Hiss declaration.
+data Decl a = Decl a (Binding a) (Expr a)
+  deriving (Eq, Show, Functor)
+
+instance Annotated Decl where
+  getAnn (Decl a _ _) = a
+
+-- | Gets Name bound by a declaration.
 declGetName :: Decl a -> Name a
 declGetName (Decl _ b _) = bindingGetName b
 
-declMapAnn :: (a -> b) -> Decl a -> Decl b
-declMapAnn f (Decl a b e) = Decl (f a) (bindingMapAnn f b) (mapAnn f e)
-
-declStripAnns :: Decl a -> Decl ()
-declStripAnns = declMapAnn (const ())
-
+-- | Hiss binary operators.
 data BinOp
-  = Add
-  | Sub
-  | Mult
-  | Div
-  | Equals
-  | NotEquals
-  | LessThan
-  | LessEqual
-  | GreaterThan
-  | GreaterEqual
-  | And
-  | Or
+  = -- | Addition operator
+    Add
+  | -- | Subtraction operator
+    Sub
+  | -- | Multiplication operator
+    Mult
+  | -- | Integer division operator
+    Div
+  | -- | Equals comparison operator
+    Equals
+  | -- | Not-equals comparison operator
+    NotEquals
+  | -- | Less-than comparison operator
+    LessThan
+  | -- | Less-than-or-equal-to comparison operator
+    LessEqual
+  | -- | Greater-than comparison operator
+    GreaterThan
+  | -- | Greater-than-or-equal-to comparison operator
+    GreaterEqual
+  | -- | Logical AND boolean operator
+    And
+  | -- | Logical OR boolean operator
+    Or
   deriving (Eq, Show)
 
+-- | Hiss unary operators.
 data UnaryOp
-  = Not
+  = -- | Logical NOT operator.
+    Not
   deriving (Eq, Show)
 
--- AST expression.
--- For Exp and other AST node types, we include a type argument a, which we can use
--- to annotate the syntax tree with, e.g., source program range information.
-data Exp a
-  = EInt a Integer -- <int>
-  | EBool a Bool -- <bool>
-  | EVar a (Name a) -- <var>
-  | EFunApp a (FunApp a) -- <exp1> <exp2> (<exp3> <exp4> ...)
-  | EUnaryOp a UnaryOp (Exp a) -- <op> <exp1>
-  | EBinOp a (Exp a) BinOp (Exp a) -- <exp1> <binop> <exp2>
-  | ELetIn a (Binding a) (Exp a) (Exp a) -- let <binding> = <exp1> in <exp2>
-  | EIf a (Exp a) (Exp a) (Exp a) -- if <exp1> then <exp2> else <exp3>
-  | EParen a (Exp a) -- (<exp1>)
-  deriving (Eq, Show, Foldable)
+-- | Hiss expression type.
+data Expr a
+  = -- | Integer literal.
+    EInt a Integer
+  | -- | Boolean literal.
+    EBool a Bool
+  | -- | Variable reference.
+    EVar a (Name a)
+  | -- | Function application.
+    EFunApp
+      a
+      (Expr a)
+      -- ^ Function expression
+      [Expr a]
+      -- ^ Zero or more argument expressions
+  | -- | Unary operator.
+    EUnaryOp a UnaryOp (Expr a)
+  | -- | Binary operator.
+    EBinOp a (Expr a) BinOp (Expr a)
+  | -- | Let..in expression.
+    ELetIn a (Binding a) (Expr a) (Expr a)
+  | -- | If-then-else expression.
+    EIf a (Expr a) (Expr a) (Expr a)
+  | -- | Parenthesized expression.
+    EParen a (Expr a)
+  deriving (Eq, Show, Functor)
 
--- Unsafely grabs annotation field from an AST node.
-getAnn :: Foldable t => t a -> a
-getAnn = fromJust . getFirst . foldMap pure
+instance Annotated Expr where
+  getAnn (EInt a _) = a
+  getAnn (EBool a _) = a
+  getAnn (EVar a _) = a
+  getAnn (EFunApp a _ _) = a
+  getAnn (EUnaryOp a _ _) = a
+  getAnn (EBinOp a _ _ _) = a
+  getAnn (ELetIn a _ _ _) = a
+  getAnn (EIf a _ _ _) = a
+  getAnn (EParen a _) = a
 
--- Recursively strips annotations from an AST.
-stripAnns :: Exp a -> Exp ()
-stripAnns = mapAnn (const ())
-
--- Recursively applies f to each AST node's annotation.
-mapAnn :: (a -> b) -> Exp a -> Exp b
-mapAnn f (EBool a b) = EBool (f a) b
-mapAnn f (EInt a i) = EInt (f a) i
-mapAnn f (EVar a n) = EVar (f a) (nameMapAnn f n)
-mapAnn f (EFunApp a funApp) = EFunApp (f a) (funAppMapAnn f funApp)
-mapAnn f (EUnaryOp a op e1) = EUnaryOp (f a) op (mapAnn f e1)
-mapAnn f (EBinOp a e1 op e2) = EBinOp (f a) (mapAnn f e1) op (mapAnn f e2)
-mapAnn f (ELetIn a lb e1 e2) = ELetIn (f a) (bindingMapAnn f lb) (mapAnn f e1) (mapAnn f e2)
-mapAnn f (EIf a e1 e2 e3) = EIf (f a) (mapAnn f e1) (mapAnn f e2) (mapAnn f e3)
-mapAnn f (EParen a e1) = EParen (f a) (mapAnn f e1)
-
--- Name of a variable.
+-- | Variable name.
 data Name a = Name a String
-  deriving (Show, Foldable)
+  deriving (Show, Functor)
 
--- Gets identifier for a Name.
-getIdent :: Name a -> String
-getIdent (Name _ s) = s
-
--- Names compare by their string value.
+-- Names compare by their identifiers.
 instance Eq (Name a) where
   (Name _ s1) == (Name _ s2) = s1 == s2
 
 instance Ord (Name a) where
   compare = comparing getIdent
 
--- Applies f to a Name's annotation.
-nameMapAnn :: (a -> b) -> Name a -> Name b
-nameMapAnn f (Name a n) = Name (f a) n
+instance Annotated Name where
+  getAnn (Name a _) = a
 
--- Variable binding, e.g., `let <binding> = <exp> in <exp>`
+-- | Gets identifier for a Name.
+getIdent :: Name a -> String
+getIdent (Name _ s) = s
+
+-- | Variable binding.
+-- E.g., `let <binding> = <exp> in <exp>`. The LHS is a binding.
 data Binding a
-  = ValBinding a (Name a) -- Value bindings are just names. e.g., 'x' in 'let x = 5 in ...'
-  | FuncBinding a (Name a) [Name a] -- Function bindings are names with zero or more arguments. e.g., 'f(a,b)' in 'let f(a,b) = a + b in ...'
-  deriving (Eq, Show, Foldable)
+  = -- | Value binding. Value bindings are just names. e.g., 'x' in 'let x = 5 in ...'
+    ValBinding a (Name a)
+  | -- | Function binding. Function bindings are names with zero or more arguments. e.g., 'f(a,b)' in 'let f(a,b) = a + b in ...'
+    FuncBinding a (Name a) [Name a]
+  deriving (Eq, Show, Functor)
 
--- Recursively applies f to a Binding's annotation and the annotations of its Names.
-bindingMapAnn :: (a -> b) -> Binding a -> Binding b
-bindingMapAnn f (ValBinding a n) = ValBinding (f a) (nameMapAnn f n)
-bindingMapAnn f (FuncBinding a n args) = FuncBinding (f a) (nameMapAnn f n) (map (nameMapAnn f) args)
+instance Annotated Binding where
+  getAnn (ValBinding a _) = a
+  getAnn (FuncBinding a _ _) = a
 
--- Gets the bound name of a Binding.
+-- | Gets the name bound by a Binding.
 bindingGetName :: Binding a -> Name a
 bindingGetName (ValBinding _ n) = n
 bindingGetName (FuncBinding _ n _) = n
-
-data FunApp a = FunApp a (Exp a) [Exp a]
-  deriving (Eq, Show, Foldable)
-
-funAppMapAnn :: (a -> b) -> FunApp a -> FunApp b
-funAppMapAnn f (FunApp a fun args) = FunApp (f a) (mapAnn f fun) (map (mapAnn f) args)
--- test/Syntax/ASTSpec.hs
+++ test/Syntax/ASTSpec.hs
@@ -5,32 +5,9 @@ import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = do
-  describe "getAnn" $ do
-    it "gets annotation from each AST node type" $ do
-      getAnn (EInt "target" 42) `shouldBe` "target"
-      getAnn (EVar "target" undefined) `shouldBe` "target"
-      getAnn (EFunApp "target" undefined) `shouldBe` "target"
-      getAnn (EBinOp "target" undefined undefined undefined) `shouldBe` "target"
-      getAnn (ELetIn "target" undefined undefined undefined) `shouldBe` "target"
-      getAnn (EIf "target" undefined undefined undefined) `shouldBe` "target"
-      getAnn (EParen "target" undefined) `shouldBe` "target"
-      getAnn (Name "target" undefined) `shouldBe` "target"
-      getAnn (ValBinding "target" undefined) `shouldBe` "target"
-      getAnn (FuncBinding "target" undefined undefined) `shouldBe` "target"
-      getAnn (FunApp "target" undefined undefined) `shouldBe` "target"
-  describe "mapAnn" $
-    it "maps annotations on a sample AST" $ -- uses integer annotations to test mapAnn
-      mapAnn
-        (* 2)
-        ( ELetIn
-            1
-            (FuncBinding 2 (Name 3 "f") [Name 4 "x"])
-            (EIf 5 (EVar 6 (Name 7 "x")) (EFunApp 8 (FunApp 9 (EVar 10 (Name 11 "f")) [EParen 12 (EBinOp 13 (EVar 14 (Name 15 "x")) Sub (EInt 16 1))])) (EInt 17 42))
-            (EFunApp 18 (FunApp 19 (EVar 20 (Name 21 "f")) [EInt 22 4])) ::
-            Exp Int
-        )
-        `shouldBe` ELetIn
-          2
-          (FuncBinding 4 (Name 6 "f") [Name 8 "x"])
-          (EIf 10 (EVar 12 (Name 14 "x")) (EFunApp 16 (FunApp 18 (EVar 20 (Name 22 "f")) [EParen 24 (EBinOp 26 (EVar 28 (Name 30 "x")) Sub (EInt 32 1))])) (EInt 34 42))
-          (EFunApp 36 (FunApp 38 (EVar 40 (Name 42 "f")) [EInt 44 4]))
+  describe "Name" $ do
+    -- ignore annotation when comparing Names
+    it "compare on identifier value" $ do
+      (Name 42 "abcde") == (Name 42 "abcde") `shouldBe` True
+      (Name 42 "abcde") == (Name 43 "abcde") `shouldBe` True
+      (Name 42 "abcde") == (Name 42 "fghij") `shouldBe` False
