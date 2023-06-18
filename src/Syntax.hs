af1f433fb6aa98207f2be5f48897e614732a6098
Joe Kerrigan <joek1301@gmail.com>
2023-06-18 11:15:58 -0400

    Update Parser and Syntax modules

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,24 +1,19 @@
 module Syntax (parseProgram, parseExpression, parseDeclaration, parseDeclOrExp) where
 
 import Error (HissError (SyntaxError))
-import Syntax.AST (Decl, Exp, Program)
+import Syntax.AST (Decl, Expr, Program)
 import Syntax.Lexer (Range, runAlex)
-import Syntax.Parser (parseDecl, parseExp, parseProg)
+import Syntax.Parser (parseDecl, parseExpr, parseProg)
 
 -- Wraps Lexer.parseProgram and returns HissError in case of failure
 parseProgram :: String -> Either HissError (Program Range)
 parseProgram inp = case runAlex inp parseProg of
   Left msg -> Left (SyntaxError msg)
-  Right ast ->
-    Right
-      ( reverse ast {- parser gives decls in reverse order, which is unimportant.
-                    but reversing them into source order gives better error messages
-                    in the case of, e.g., redeclaration errors -}
-      )
+  Right prog -> Right prog
 
 -- Wraps Lexer.parseExp and returns HissError in case of failure
-parseExpression :: String -> Either HissError (Exp Range)
-parseExpression inp = case runAlex inp parseExp of
+parseExpression :: String -> Either HissError (Expr Range)
+parseExpression inp = case runAlex inp parseExpr of
   Left msg -> Left (SyntaxError msg)
   Right ast -> Right ast
 
@@ -30,7 +25,7 @@ parseDeclaration inp = case runAlex inp parseDecl of
 
 -- | Tries to parse 'str' as a declaration.
 -- | If that fails, tries to parse 'str' as an expression.
-parseDeclOrExp :: String -> Either HissError (Either (Decl Range) (Exp Range))
+parseDeclOrExp :: String -> Either HissError (Either (Decl Range) (Expr Range))
 parseDeclOrExp inp = case parseDeclaration inp of
   Right decl -> return (Left decl)
   _ -> do
