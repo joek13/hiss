7931ab64beced8dc645d4cccbd17b76b4228d5a3
Joe Kerrigan <joek1301@gmail.com>
2023-07-06 20:11:53 -0400

    Finish first pass at HM type system

--- src/Semantic/Types/Constraints.hs
+++ src/Semantic/Types/Constraints.hs
@@ -1,7 +1,8 @@
 module Semantic.Types.Constraints where
 
+import Control.Monad (replicateM)
 import Control.Monad.Except (Except, runExcept)
-import Control.Monad.RWS (MonadReader (ask), MonadWriter (tell), RWST (runRWST), gets, modify)
+import Control.Monad.RWS (MonadReader (ask, local), MonadWriter (tell), RWST (runRWST), gets, modify)
 import Data.Map qualified as Map
 import Data.Set qualified as Set
 import Error (HissError)
@@ -9,13 +10,13 @@ import Semantic.Types
   ( Cons (CBool, CInt),
     Scheme (..),
     Substitutable (..),
-    Type (TCons, TVar),
+    Type (TCons, TFunc, TVar),
     TypeEnv,
     Var (Var),
     varNames,
   )
-import Syntax.AST (Expr (EBool, EInt, EVar), Name, getIdent)
-import Syntax.Lexer
+import Syntax.AST (BinOp (..), Binding (..), Expr (..), Name, UnaryOp (..), getIdent)
+import Syntax.Lexer (Range)
 
 -- | Given a type env and name, looks up its type.
 lookupName :: Name Range -> Infer Type
@@ -34,6 +35,7 @@ initCounter = Counter {count = 0}
 
 -- | Type equality constraint.
 newtype Constraint = TyEq (Type, Type)
+  deriving (Eq, Show)
 
 instance Substitutable Constraint where
   apply s1 (TyEq (t1, t2)) = TyEq (apply s1 t1, apply s1 t2)
@@ -53,6 +55,13 @@ fresh = do
   i <- gets count
   modify $ Counter . (+ 1) . count -- increment counter
   return $ TVar (Var $ varNames !! i) -- index into list of type variable names
+
+-- | Binds a name to a scheme and performs an Infer action in the modified environment.
+bindEnv :: (Name Range, Scheme) -> Infer a -> Infer a
+bindEnv (name, scheme) = local (Map.insert name scheme)
+
+bindEnvMany :: [(Name Range, Scheme)] -> Infer a -> Infer a
+bindEnvMany pairs = local (\e -> foldl (\m (n, s) -> Map.insert n s m) e pairs)
 
 -- | Instantiates a type scheme, replacing its bound variables
 -- with fresh type variables.
@@ -78,7 +87,102 @@ infer expr = case expr of
   EInt _ _ -> return $ TCons CInt
   EBool _ _ -> return $ TCons CBool
   EVar _ n -> lookupName n
-  _ -> error "Unimplemented!"
+  EFunApp _ funExpr argExprs -> do
+    funTy <- infer funExpr -- type of the function
+    argTys <- mapM infer argExprs -- list of arguent types
+
+    -- fresh variable to represent return type
+    retTy <- fresh
+    -- funExpr *should* have this type. matching argument types, return type
+    let funTy' = TFunc argTys retTy
+    constrain funTy funTy'
+
+    return retTy
+  EUnaryOp _ op subexpr -> inferUnary op subexpr
+  EBinOp _ expr1 op expr2 -> inferBinary op expr1 expr2
+  ELetIn _ binding valExpr inExpr -> case binding of
+    ValBinding _ n -> do
+      valTy <- infer valExpr
+      valSc <- generalize valTy
+      bindEnv (n, valSc) (infer inExpr)
+    FuncBinding _ funcName argNames -> do
+      funcSc <- inferFunc funcName argNames valExpr
+      bindEnv (funcName, funcSc) (infer inExpr)
+  EIf _ condExpr thenExpr elseExpr -> do
+    -- condition must be bool
+    condTy <- infer condExpr
+    constrain condTy (TCons CBool)
+
+    -- then/else must have matching type
+    thenTy <- infer thenExpr
+    elseTy <- infer elseExpr
+    constrain thenTy elseTy
+
+    return thenTy
+  EParen _ subexpr -> infer subexpr
+
+inferUnary :: UnaryOp -> Expr Range -> Infer Type
+inferUnary op expr = case op of
+  Not -> do
+    exprTy <- infer expr
+    constrain exprTy (TCons CBool)
+    return $ TCons CBool
+
+inferBinary :: BinOp -> Expr Range -> Expr Range -> Infer Type
+inferBinary op expr1 expr2 = case op of
+  Add -> inferArith
+  Sub -> inferArith
+  Mult -> inferArith
+  Div -> inferArith
+  Equals -> inferCmp
+  NotEquals -> inferCmp
+  GreaterThan -> inferCmp
+  GreaterEqual -> inferCmp
+  LessThan -> inferCmp
+  LessEqual -> inferCmp
+  And -> inferLogical
+  Or -> inferLogical
+  where
+    inferArith = do
+      expr1Ty <- infer expr1
+      expr2Ty <- infer expr2
+      constrain expr1Ty (TCons CInt)
+      constrain expr2Ty (TCons CInt)
+      return $ TCons CInt
+    inferCmp = do
+      expr1Ty <- infer expr1
+      expr2Ty <- infer expr2
+      constrain expr1Ty (TCons CInt)
+      constrain expr2Ty (TCons CInt)
+      return $ TCons CBool
+    inferLogical = do
+      expr1Ty <- infer expr1
+      expr2Ty <- infer expr2
+      constrain expr1Ty (TCons CBool)
+      constrain expr2Ty (TCons CBool)
+      return $ TCons CBool
+
+inferFunc :: Name Range -> [Name Range] -> Expr Range -> Infer Scheme
+inferFunc funcName argNames defnExpr = do
+  -- create fresh type variables for each function argument
+  argTys <- replicateM (length argNames) fresh
+  let argScs = map (ForAll []) argTys
+
+  -- fresh variable for ret type
+  retTy <- fresh
+
+  let funcTy = TFunc argTys retTy -- construct the function type
+  let funcSc = ForAll [] funcTy
+  let argPairs = zip argNames argScs -- list of (arg name, arg schema)
+
+  -- infer the return type of the function from the body
+  retTy' <-
+    bindEnvMany
+      ((funcName, funcSc) : argPairs) -- include itself since functions can be recursive
+      (infer defnExpr)
+  constrain retTy retTy'
+
+  return funcSc
 
 runInfer :: TypeEnv -> Infer a -> Either HissError (a, [Constraint])
 runInfer env m = do
