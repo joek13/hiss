26bc5c38d20618b45a671420bd2bfc2b20cb5c9e
Joe Kerrigan <joek1301@gmail.com>
2023-07-10 00:13:43 -0400

    Use relabel

--- src/Semantic/Typechecking.hs
+++ src/Semantic/Typechecking.hs
@@ -5,7 +5,7 @@ module Semantic.Typechecking (infer, infer') where
 
 import Data.Map qualified as Map
 import Error (HissError)
-import Semantic.Types (Substitutable (apply), Type, TypeEnv)
+import Semantic.Types (Substitutable (apply), Type, TypeEnv, relabel)
 import Semantic.Types.Constraints (runInfer, solve)
 import Semantic.Types.Constraints qualified as Constraints (infer)
 import Syntax.AST (Expr)
@@ -18,7 +18,10 @@ infer env expr = do
   -- solve for type substitutions
   subst <- solve cs
   -- apply substitutions to the type
-  return $ apply subst ty
+  let solved = apply subst ty
+  -- and relabel vars
+  let relabeled = relabel solved
+  return relabeled
 
 infer' :: Expr Range -> Either HissError Type
 infer' = infer Map.empty
