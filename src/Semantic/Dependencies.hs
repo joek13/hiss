75cfa18c5a75f306d8a202c834694c09f4b99cc8
Joe Kerrigan <joek1301@gmail.com>
2023-07-27 22:42:44 -0700

    Simplify reorderDecls

--- src/Semantic/Dependencies.hs
+++ src/Semantic/Dependencies.hs
@@ -3,25 +3,22 @@
     2. Semantic pass to detect value cycles.
 -}
 
-module Semantic.Dependencies (dependencyGraph, reorderDecls) where
+module Semantic.Dependencies (reorderDecls) where
 
-import Control.Monad (unless, when)
-import Control.Monad.Except (ExceptT, MonadError (throwError), runExceptT)
-import Control.Monad.State (MonadState (get, put), State, evalState, gets, modify)
 import Data.Foldable (find)
 import Data.Foldable qualified as Foldable (toList)
 import Data.Function ((&))
-import Data.Graph (graphFromEdges, scc, Vertex, Graph)
+import Data.Graph (Graph, Vertex, graphFromEdges, scc)
 import Data.Map (Map)
 import Data.Map qualified as Map (fromList, lookup)
 import Data.Maybe (catMaybes, fromJust)
-import Data.Set (Set, (\\))
-import Data.Set qualified as Set (empty, fromList, insert, map, member, singleton, toList, union)
+import Data.Set ((\\))
+import Data.Set qualified as Set (fromList, map, singleton, toList, union)
 import Error (HissError (SemanticError))
-import Syntax (getLineCol, start)
-import Syntax.AST (Annotated (getAnn), Binding (..), Decl (..), Expr (..), Name (..), Program (..), declGetName, getIdent, progDecls)
-import Syntax.Lexer (Range)
 import Semantic.Names (collectNames)
+import Syntax (getLineCol, start)
+import Syntax.AST (Annotated (getAnn), Binding (..), Decl (..), Name (..), Program (..), declGetName, getIdent, progDecls)
+import Syntax.Lexer (Range)
 
 -- | Given (name -> vertex) map and a declaration, creates an adjacency list entry
 --   The node is the declaration itself, and the adjacency list tracks dependencies on other declarations.
@@ -56,40 +53,42 @@ dependencyGraph prog =
     -- Extracts declaration from adjacency list node.
     declFromNode (decl, _, _) = decl
 
--- | Re-orders a program's declarations to support program interpretation.
---   Declarations are reorganized into dependency order so that values are
---   always declared lexically before they are used.
---   If this is impossible (i.e. there is a cyclic definition), returns an error.
-reorderDecls :: Program Range -> Either HissError (Program Range)
-reorderDecls prog = do
+-- | Groups a program's declarations into sets of mutually dependent declarations.
+--   Equivalently, finds strongly connected components of the program dependency graph.
+--   Returns list of strongly connected components in reverse topological order.
+groupDecls :: Program a -> [[Decl a]]
+groupDecls prog =
   let (graph, declFromVertex) = dependencyGraph prog
-
-  let sccs =
+      sccs =
         scc graph -- find strongly connected components (SCCs)
           & map (fmap declFromVertex) -- convert from Forest Vertex -> Forest (Decl Range)
+   in map Foldable.toList sccs
 
-  {- in general, a graph is acyclic <=> each SCC contains exactly one vertex
-     In our case, we only care about dependency cycles containing value declarations,
-     since we allow mutually recursive functions
-   -}
-  let cyclic = find isCyclic sccs
-  -- any dependency cycles containing values?
-  case cyclic of
-    Just tree ->
-      -- if yes, find a value to report (since tree root might be a function declaration)
-      let decl = fromJust $ find isValDecl tree
+-- | Reorganizes a program's declarations into dependency order.
+--   Reorders declarations so that values are always defined lexically before they are used.
+--   If this is impossible (i.e., value has cyclic definition), returns an error.
+reorderDecls :: Program Range -> Either HissError (Program Range)
+reorderDecls prog =
+  case find isCyclic groupedDecls of
+    Just group ->
+      -- if yes, find a value to report (first value might be a function)
+      let decl = fromJust $ find isValDecl group
           name = declGetName decl
           (line, col) = (getLineCol . start . getAnn) name
        in Left $ SemanticError $ "Definition of '" <> getIdent name <> "' at line " <> show line <> ", column " <> show col <> " is cyclic"
-    -- if no, return the program in dependency order
-    Nothing -> return $ Program (getAnn prog) $ concatMap Foldable.toList sccs
+    Nothing -> return $ Program (getAnn prog) $ concat groupedDecls
   where
+    groupedDecls = groupDecls prog
     --  Returns True if its argument is a function declaration.
     isFuncDecl (Decl _ (FuncBinding {}) _) = True
     isFuncDecl _ = False
     -- Returns True if its argument is a value declaration.
     isValDecl = not . isFuncDecl
-    -- Returns True if its argument has more than one node and at least one is a value declaration.
-    isCyclic tree = (length tree > 1) && any isValDecl tree
-    -- Extracts declaration from adjacency list node.
-    declFromNode (decl, _, _) = decl
+    {-
+      in general, a graph is acyclic <=> each SCC contains exactly one node.
+      we only care about detecting cyclic definitions for values
+      (cyclic function definitions are just mutual recursion, which is allowed)
+
+      isCyclic returns True if its argument has more than one node and at least one is a value declaration.
+    -}
+    isCyclic group = (length group > 1) && any isValDecl group
