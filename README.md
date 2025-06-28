beaea5e37e96b10c31a79b12a9541be319e78078
Joe Kerrigan <joek1301@gmail.com>
2025-06-28 17:02:13 -0500

    refactor CLI

--- README.md
+++ README.md
@@ -6,21 +6,20 @@ This repository contains the source code for the Hiss compiler `hissc`.
 
 ## Progress
 - [x] Parser/lexer
-- [ ] Semantic passes
+- [x] Semantic passes
     - [x] Name checker
     - [x] Dependency checker
     - [x] Type checker
-- [x] Simple tree-walk interpreter
-- [ ] Codegen
+- [x] Codegen
 
 ## Commands
 ```bash
 # build the project
 stack build
-# interpret a hiss program
-stack run -- eval samples/collatz.hiss
-# start a hiss REPL
-stack run -- repl samples/collatz.hiss
+# compile a hiss program
+stack run -- samples/fib.hiss
+# emit human readable assembly
+stack run -- samples/fib.hiss --asm
 # run unit tests
 stack test
 ```
--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,92 @@
 module Main (main) where
 
-import Command (Command (..))
-import Command.Eval (doEval, evalOptsParser)
-import Command.Repl (doRepl, replOptsParser)
-import Command.Codegen (doCodegen, codegenOptsParser)
-import Options.Applicative (Parser, ParserInfo, command, execParser, fullDesc, header, helper, info, progDesc, subparser, (<**>))
+import Error ( HissError, HissError, showErr )
+import Codegen.Program (Program)
+import Options.Applicative (Parser, ParserInfo, execParser, fullDesc, header, helper, info, progDesc, (<**>))
+import Options.Applicative.Builder (value, argument, long, short, metavar, help, str, option, flag, ReadM, maybeReader)
+import Semantic.Dependencies (reorderDecls)
+import Semantic.Names (checkNames)
+import Semantic.Typechecking (Typecheck (..))
+import Syntax (parseProgram)
+import Codegen (codegen)
+import qualified Data.ByteString as B
+import Codegen.Assembly (writeAssembly)
+import Assembler (assemble)
+
+
+data OutputFormat = Assembly | Bytecode
+
+-- Function that derives an output path from a format and input path.
+type OutPath = OutputFormat -> String -> String
 
 -- Global hissc options.
-newtype Options = Options {optCommand :: Command}
+data Options = Options { inputPath :: String, outPath :: OutPath , format :: OutputFormat }
 
-optsParser' :: Parser Options
-optsParser' =
-  Options
-    <$> subparser
-      ( command "eval" evalOptsParser
-          <> command "repl" replOptsParser
-          <> command "codegen" codegenOptsParser
-      )
+fileName :: String -> String
+fileName = reverse . takeWhile (/= '/') . reverse
 
-optsParser :: ParserInfo Options
-optsParser =
+removeExtension :: String -> String
+removeExtension = reverse . drop 1 . dropWhile (/= '.') . reverse
+
+defaultOutPath :: OutPath
+defaultOutPath Assembly = (<>".hissa") . removeExtension . fileName
+defaultOutPath Bytecode = (<>".hissc") . removeExtension . fileName
+
+readOutPath :: ReadM OutPath
+readOutPath = maybeReader f
+  where f s = Just $ curry $ const s
+
+parser :: Parser Options
+parser = Options
+  <$> argument str (metavar "INPATH" <> help "Path to input Hiss program.")
+  <*> option readOutPath (long "output" <> short 'o' <> metavar "OUTPATH" <> value defaultOutPath <> help "Output path. (Default: derived from input filename.)")
+  <*> flag Bytecode Assembly (long "asm" <> short 'a' <> help "Emit Hiss assembly. (Default: emit bytecode.)")
+
+
+parserInfo :: ParserInfo Options
+parserInfo =
   info
-    (optsParser' <**> helper)
+    (parser <**> helper)
     (fullDesc <> progDesc "Compiler for the Hiss programming language." <> header "hissc - Hiss compiler")
+
+compile :: String -> Either HissError Program
+compile source =
+  -- Lex/parse program.
+  parseProgram source
+      -- Check validity of program identifiers.
+      >>= checkNames
+      -- Reorder program declarations so values are always declared before they are used.
+      >>= reorderDecls
+      -- Run type check.
+      >>= typecheck
+      -- Generate code and strip range information.
+      >>= codegen . fmap snd
+
+write :: OutputFormat -> String -> Program -> IO ()
+write Assembly outPath prog = 
+  let result = writeAssembly prog in
+  case result of 
+    Right asm -> do
+      writeFile outPath (unlines asm)
+      putStrLn $ "Wrote assembly to " <> outPath
+    Left err -> putStrLn $ showErr err
+write Bytecode outPath prog = 
+  let result = assemble prog in
+  case result of 
+    Right bytecode ->  do
+      B.writeFile outPath bytecode
+      putStrLn $ "Wrote bytecode to " <> outPath
+    Left err -> putStrLn $ showErr err
 
 main :: IO ()
 main = do
-  opts <- execParser optsParser
-  case optCommand opts of
-    Eval evalOpts -> doEval evalOpts
-    Repl replOpts -> doRepl replOpts
-    Codegen codegenOpts -> doCodegen codegenOpts
+  opts <- execParser parserInfo
+  let sourcePath = inputPath opts
+  source <- readFile sourcePath
+  case compile source of
+    Left err -> putStrLn $ showErr err
+    Right program -> do
+      putStrLn $ "Successfully compiled " <> sourcePath
+      let fmt = format opts
+      let out = outPath opts fmt (inputPath opts)
+      write fmt out program
